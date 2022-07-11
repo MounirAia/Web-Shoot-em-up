fe888fc56be40729d343256825e8f3300ed0c7d2
Mounir Aiache <aiamounirpro@gmail.com>
2022-07-11 16:07:44 -0400

    wave generate enemies in random order

--- src/WaveManager/WaveEnemies.ts
+++ src/WaveManager/WaveEnemies.ts
@@ -1,40 +1,72 @@
 import { canvas, CANVA_SCALEX } from '../ScreenConstant.js';
+import { CircleEnemy } from '../Sprites/Enemies/Circle/CircleEnemy.js';
 import { BigDiamondEnemy } from '../Sprites/Enemies/Diamond/BigDiamondEnemy.js';
 import { IEnemy } from '../Sprites/Enemies/IEnemy.js';
+import { RectangleEnemy } from '../Sprites/Enemies/Rectangle/RectangleEnemy.js';
+import { TrapezeEnemy } from '../Sprites/Enemies/Trapeze/TrapezeEnemy.js';
+import { TriangleEnemy } from '../Sprites/Enemies/Triangle/TriangleEnemy.js';
 import { ISpriteWithHitboxes } from '../Sprites/InterfaceBehaviour/ISpriteWithHitboxes.js';
 
-// have to test this new setting with all the enemies
 // have to add randomness in the type of enemies generated (bag method)
+
+function generateRandomNumberArray(max: number): number[] {
+    const indicesMonster: number[] = [];
+    // fill the array
+    for (let index = 0; index < max; index++) {
+        indicesMonster.push(index);
+    }
+    // shuffle the array
+    let tmp,
+        current,
+        top = indicesMonster.length;
+    if (top)
+        while (--top) {
+            current = Math.floor(Math.random() * (top + 1));
+            tmp = indicesMonster[current];
+            indicesMonster[current] = indicesMonster[top];
+            indicesMonster[top] = tmp;
+        }
+    return indicesMonster;
+}
 
 export class WaveEnemies {
     private listEnemies: Map<IEnemy, IEnemy>;
-    private readonly numberSpawns;
+    private readonly numberSpawns = 8;
     private readonly maxNumberEnemies = 40;
+    private readonly numberTypeOfRegularEnemies = 5;
     constructor(numberEnemies: number) {
         if (numberEnemies > this.maxNumberEnemies) numberEnemies = this.maxNumberEnemies;
 
         this.listEnemies = new Map<IEnemy, IEnemy>();
-        this.numberSpawns = 8;
+        this.createWave(numberEnemies);
+    }
+
+    private createWave(numberEnemies: number) {
         const verticalGapBetweenMonsters = 60;
         const verticalShift = (canvas.height - this.numberSpawns * verticalGapBetweenMonsters) / 2; // vertical padding in columns
         let currentNumberEnemy = 0;
         let x = canvas.width;
         let y = 0;
-
         const monsterScreenWidthProportion = canvas.width * (30 / 100); // how much screen the monsters shooting position take
         const maxNumberShootingSpawn = 5;
         const horizontalGapBetweenMonsters = Math.floor(monsterScreenWidthProportion / maxNumberShootingSpawn);
         let monsterShootingPosition = canvas.width - monsterScreenWidthProportion; // where the monsters will be
-
+        let randomIndicesMonster = generateRandomNumberArray(this.numberTypeOfRegularEnemies);
         while (currentNumberEnemy < numberEnemies) {
             for (let index = 0; index < this.numberSpawns; index++) {
                 currentNumberEnemy++;
-                const ennemy = new BigDiamondEnemy(
-                    x,
-                    verticalShift + (y % (this.numberSpawns * verticalGapBetweenMonsters)),
-                    monsterShootingPosition,
+
+                if (!randomIndicesMonster.length)
+                    randomIndicesMonster = generateRandomNumberArray(this.numberTypeOfRegularEnemies);
+
+                this.AddEnemy(
+                    this.generateEnemy(
+                        randomIndicesMonster.pop()!,
+                        x,
+                        verticalShift + (y % (this.numberSpawns * verticalGapBetweenMonsters)),
+                        monsterShootingPosition,
+                    ),
                 );
-                this.AddEnemy(ennemy);
                 y += verticalGapBetweenMonsters;
 
                 if (currentNumberEnemy >= numberEnemies) {
@@ -45,6 +77,25 @@ export class WaveEnemies {
             x += horizontalGapBetweenMonsters;
             y = 0;
         }
+    }
+
+    private generateEnemy(index: number, x: number, y: number, monsterShootingPosition: number): IEnemy {
+        switch (index) {
+            case 0:
+                return new CircleEnemy(x, y, monsterShootingPosition);
+            case 1:
+                return new BigDiamondEnemy(x, y, monsterShootingPosition);
+            case 2:
+                return new RectangleEnemy(x, y, monsterShootingPosition);
+            case 3:
+                return new TrapezeEnemy(x, y, monsterShootingPosition);
+            case 4:
+                return new TriangleEnemy(x, y, monsterShootingPosition);
+            default:
+                break;
+        }
+
+        return new BigDiamondEnemy(1, 1, 1);
     }
 
     public AddEnemy(enemy: IEnemy) {
@@ -110,3 +161,9 @@ export class WaveEnemies {
 
 // Change setting for an enemy waves to stop in predefined spawns, how many enemies that will be generated and so on
 // then stop the enemy spawns
+
+// random enemies generation is made like so:
+// you have 5 enemies type, each 5 enemies batch you
+// spawn them randomly.If you have 12 enemies to spawn you do (the number 5 correspond to the number of enemies type)
+// 12 - 5 = 7 (you spawn 5 enemies randomly), 7 - 5 = 2 (you spawn 5 enemies randomly)
+// 2 -2 (you spawn 2 enemies randomly)
