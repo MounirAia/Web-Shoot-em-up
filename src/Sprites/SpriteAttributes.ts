a141e6e9564a663a2a0ccfeb98b6f40802d22bd2
Mounir Aiache <aiamounirpro@gmail.com>
2024-07-10 11:08:59 -0400

    refactor: Update PlayerBullet class to use new sprite info file

--- src/Sprites/SpriteAttributes.ts
+++ src/Sprites/SpriteAttributes.ts
@@ -28,27 +28,16 @@ export interface ISpriteWithDamageResistance {
     DamageResistancesController: SpriteDamageResistancesController;
 }
 
-// if a sprite has a stat that can influence the damage he does
-export interface ISpriteWithDamageUpgrades {
-    DamageUpgrades: number[];
-}
-
 export interface ISpriteWithHealth {
     BaseHealth: number;
     MaxHealth?: number;
     CurrentHealth: number;
-}
-export interface ISpriteWithHealthUpgrades {
-    HealthUpgrades: number[];
 }
 
 export interface ISpriteWithAttackSpeed {
     BaseAttackSpeed: number;
     AttackSpeed: number;
     CanShootRegular?: boolean;
-}
-export interface ISpriteWithAttackSpeedUpgrades {
-    AttackSpeedUpgrades: number[];
 }
 
 // Make a sprite movable, by enforcing it to have a speed (pixel/second)
